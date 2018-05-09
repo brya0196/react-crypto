9061c1d029c8d279097d6ab312a97f55a4bd96d6
Bryan Astacio <bryanastacio@MacBook-Pro-de-Bryan.local>
2018-05-08 23:11:08 -0400

    added the axios pacakge to make the request end used the on componentDidMount to make the request, also added react-number-format to format the values that come from the api server

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import NumberFormat from 'react-number-format';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      cryptos: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,DASH&tsyms=USD')
+    .then(response => {
+      const cryptos = response.data
+      this.setState({ cryptos: cryptos });
+    })
+    .catch(e => console.log(e));
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {Object.keys(this.state.cryptos).map((key) => (
+          <div id="crypto-container">
+            <span className="left">{key}</span>
+            <span className="right"><NumberFormat value={this.state.cryptos[key].USD} displayType={'text'} decimalPrecision={2} thousandSeparator={true} prefix={'$'}  /></span>
+          </div>
+        ))}
       </div>
     );
   }
